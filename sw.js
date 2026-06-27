1e3db94c4744a1c1c403a7cffb390ac1c4c81678
m0423w0422-create <m0423.w0422@gmail.com>
2026-06-27 12:53:43 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "gtd-v7";
+const CACHE = "gtd-v8";
 
 self.addEventListener("install", e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(["./","./index.html"])).catch(()=>{}));
